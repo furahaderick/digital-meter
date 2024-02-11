41df008d8df9cdd470fc68fe5eda48a3dc6fddcd
furahaderick <yvesderricks@gmail.com>
2024-02-11 22:24:08 +0200

    feat: Use opencv and tesseract to read text from image

---
+++ app.py
@@ -0,0 +1,20 @@
+# Import the necessary libraries 
+import cv2 
+import pytesseract 
+import numpy as np
+
+img = cv2.imread('./data/reading.png') 
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+blur = cv2.GaussianBlur(gray, (5,5), 0) 
+ 
+bin_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+ 
+kernel = np.ones((1, 1), np.uint8) 
+img = cv2.dilate(bin_img, kernel, iterations=1) 
+img = cv2.erode(img, kernel, iterations=1) 
+
+text = pytesseract.image_to_string(img) 
+
+print(text)
